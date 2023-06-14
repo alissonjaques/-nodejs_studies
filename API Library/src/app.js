a91aa55d3ba9e16a8158c6bc20e8db96fded274a
Alisson Jaques - Computer Engineer <alissonjaquesrmq@gmail.com>
2023-06-13 21:19:14 -0300

    Testing different types of http requests

--- API Library/src/app.js
+++ API Library/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 const books = [
   { id: 1, titulo: "Senhor dos Anéis" },
@@ -8,12 +9,52 @@ const books = [
   { id: 3, titulo: "Harry Potter e a Ordem da Fênix" },
 ];
 
-app.get("/", (req, resp) => {
-  resp.status(200).send("Curso de Node");
+app.get("/", (req, res) => {
+  res.status(200).send("Curso de Node");
 });
 
-app.get("/livros", (req, resp) => {
-  resp.status(200).json(books);
+app.get("/livros", (req, res) => {
+  res.status(200).json(books);
 });
+
+app.get("/livros/:id", (req, res) => {
+  const index = getBook(req.params.id);
+  if (index == -1) {
+    res.status(404).send(`Livro de id = ${req.params.id} não encontrado!`);
+  } else {
+    res.status(200).json(books[index]);
+  }
+});
+
+app.post("/livros", (req, res) => {
+  books.push(req.body);
+  res.status(201).send("Livro cadastrado com sucesso!");
+});
+
+app.put("/livros/:id", (req, res) => {
+  const index = getBook(req.params.id);
+  if (index == -1) {
+    res.status(404).send(`Livro de id = ${req.params.id} não encontrado!`);
+  } else {
+    books[index].titulo = req.body.titulo;
+    res.status(204).json(books[index]);
+  }
+});
+
+app.delete("/livros/:id", (req, res) => {
+  const { id } = req.params;
+  const index = getBook(id);
+  if (index == -1) {
+    res.status(404).send(`Livro de id = ${req.params.id} não encontrado!`);
+  } else {
+    const book = books[index];
+    books.splice(index, 1);
+    res.status(200).json(book);
+  }
+});
+
+function getBook(id) {
+  return books.findIndex((book) => book.id == id);
+}
 
 export default app;
