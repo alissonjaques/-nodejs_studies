29a036fe1f04f9173047de272b996ee7e30bf0b4
Alisson Jaques - Computer Engineer <alissonjaquesrmq@gmail.com>
2023-08-15 14:12:38 -0300

    Added functionality to change avatar image

---
+++ api-sales/src/modules/users/controllers/UserAvatarController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+import AppError from '@shared/errors/AppError';
+
+export default class UserAvatarController {
+  public async update(request: Request, response: Response): Promise<Response> {
+    const updateAvatar = new UpdateUserAvatarService();
+
+    if (request.file?.filename) {
+      const user = updateAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+      return response.json(user);
+    }
+
+    throw new AppError('Internal Server Error', 500);
+  }
+}
--- api-sales/src/shared/http/server.ts
+++ api-sales/src/shared/http/server.ts
@@ -4,15 +4,18 @@ import 'express-async-errors';
 import cors from 'cors';
 import { errors } from 'celebrate';
 import routes from './routes';
-import AppError from '../errors/AppError';
+import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
+import uploadConfig from '@config/upload';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 app.use(errors());
+
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
@@ -21,7 +24,6 @@ app.use(
         message: error.message,
       });
     }
-
     return response.status(500).json({
       status: 'error',
       message: 'Internal server error',
