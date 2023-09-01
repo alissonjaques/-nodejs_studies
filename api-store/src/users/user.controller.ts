3c27b460153ffd50e808c4122fd23234ba7422dc
Alisson Jaques <alisson.jaques@squadra.com.br>
2023-09-01 13:02:08 -0300

    API-Store feat: listing and creating users

---
+++ api-store/src/users/user.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UserRepository } from './user.repository';
+
+@Controller('/usuarios')
+export class UserController {
+  private userRepository = new UserRepository();
+
+  @Post()
+  async createUser(@Body() userData) {
+    await this.userRepository.save(userData);
+  }
+
+  @Get()
+  async listUser() {
+    return await this.userRepository.list();
+  }
+}
