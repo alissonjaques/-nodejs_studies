1e55c64e9034315a77f54829d1a1baad09304ea5
Alisson Jaques <alissonjaquesrmq@gmail.com>
2023-09-04 15:26:46 -0300

    API-Store refactor: adjustments

--- api-store/src/app.module.ts
+++ api-store/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './users/user.module';
 import { ProductModule } from './products/product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/postgres.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, ProductModule],
+  imports: [
+    UserModule,
+    ProductModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
